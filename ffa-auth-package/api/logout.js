2cd6e2077fe4adcc9ee53bc03fdf881fc7d30787
RuthreshwaranM <wruthresh@gmail.com>
2026-07-21 09:55:41 +0530

    Delete ffa-auth-package directory

--- ffa-auth-package/api/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearCookie } = require('./_lib/auth');
-
-module.exports = async (req, res) => {
-  clearCookie(res);
-  return res.status(200).json({ success: true, message: 'Logged out successfully' });
-};
